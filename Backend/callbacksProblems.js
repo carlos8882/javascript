dc60c53f22eeddabdfd3f80300742813cc2fe4cb
Carlos Camacho Soria <Carlos Camacho@jalasoft.local>
2020-03-23 10:55:11 -0400

    callbacks problems and promises

---
+++ Backend/callbacksProblems.js
@@ -0,0 +1,26 @@
+// a common problem with callbacks, is that can appear the pyramid of doom or callback hell
+// thats are referred to horizontal pyramid that makes the code difficult to read
+
+loadScript('1.js', function(error, script) {
+
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('2.js', function(error, script) {
+      if (error) {
+        handleError(error);
+      } else {
+        // ...
+        loadScript('3.js', function(error, script) {
+          if (error) {
+            handleError(error);
+          } else {
+            // ...continue after all scripts are loaded (*)
+          }
+        });
+
+      }
+    })
+  }
+});
---
+++ Backend/promises.js
@@ -0,0 +1,6 @@
+//in order to avoid the pyramide of doom, the user is able to use promises
+//using promises the user is able to read the code better
+// a promise can be return a good response or an error (resolve, reject)
+let promise = new Promise(function(resolve, reject){
+
+});
